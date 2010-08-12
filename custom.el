8fa14e655d4138c6cac5fd73d688654104bb6e86
Rafael B. Tauil <rafael@tauil.com.br>
2010-08-12 19:30:13 -0300

    Reorganized lisp libs paths

--- custom.el
+++ custom.el
@@ -11,46 +11,42 @@
 (set-keyboard-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 
+;; Add libs to path -----------------------------------------------------------
+(add-to-list 'load-path "~/Projetos/emacs-designer-kit/lib")
+
 ;; Show line numbers ----------------------------------------------------------
-(load-file "~/Projetos/emacs-designer-kit/linum.el")
 (require 'linum)
 (global-linum-mode 1)
 
-;; Textmate mode -------------
-(add-to-list 'load-path "~/Projetos/emacs-designer-kit/textmate.el")
+;; Textmate mode --------------------------------------------------------------
 (require 'textmate)
 (textmate-mode)
 
+;; Interactively Do Things ----------------------------------------------------
+(require 'ido)
+(ido-mode t)
+
 ;; Code auto-complete ---------------------------------------------------------
 (autoload 'html-helper-mode "html-helper-mode" "Yay HTML" t)
-;(setq auto-mode-alist (cons '("\\.html$" . html-helper-mode) auto-mode-alist))
 (load-file "~/Projetos/yasnippet/yasnippet.el")
 (require 'yasnippet)
 (yas/initialize)
 (yas/load-directory "~/Projetos/yasnippet/snippets")
 `
-;; Add modes ---------------------------------------------------------------
-(setq auto-mode-alist       
-     (cons '("\\.haml\\'" . text-mode) auto-mode-alist))
-
-(load-file "~/Projetos/emacs-designer-kit/css-mode.el")
-(autoload 'css-mode "css-mode")
-(setq auto-mode-alist       
-     (cons '("\\.css\\'" . css-mode) auto-mode-alist))
-(setq cssm-indent-function #'cssm-c-style-indenter)
-
-;; Aquamacs setup ------------------------------------------------------------------
+;; Aquamacs setup -------------------------------------------------------------
 (custom-set-variables
- '(global-hl-line-mode t)
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-customization-version-id 190 t)
+ '(default-frame-alist (quote ((tool-bar-lines . 0) (menu-bar-lines . 1) (cursor-type . box) (vertical-scroll-bars . right) (internal-border-width . 0) (left-fringe . 1) (right-fringe) (fringe) (background-color . "black") (background-mode . dark) (border-color . "black") (cursor-color . "#FF9900") (foreground-color . "#CCCCCC") (mouse-color . "sienna1"))))
  '(indent-tabs-mode nil)
  '(setq standard-indent)
  '(tab-width 2)
  '(tabbar-mode nil nil (tabbar))
  '(transient-mark-mode t))
-
-;; Interactively Do Things ----------------------------------------------------
-(require 'ido)
-(ido-mode t)
  
 ;; Load Color Theme -----------------------------------------------------------
 (add-to-list 'load-path "~/Projetos/emacs-designer-kit/color-theme/")
@@ -64,7 +60,9 @@
 (color-theme-schwarz)
 (load-file "~/Projetos/emacs-designer-kit/color-theme/themes/color-theme-schwarz.el")
 
+;; Backgroud transparency and current line color ------------------------------
 (modify-frame-parameters (selected-frame) '((alpha . 85)))
+(global-hl-line-mode 1)
 (set-face-background 'hl-line "#333")
 
 ;;  Misc ----------------------------------------------------------------------
@@ -90,12 +88,24 @@
 
 (add-to-list 'auto-mode-alist '("COMMIT_EDITMSG$" . diff-mode))
 (add-to-list 'auto-mode-alist '("\\.css$" . css-mode))
+;;(setq auto-mode-alist (cons '("\\.css\\'" . css-mode) auto-mode-alist))
 (add-to-list 'auto-mode-alist '("\\.ya?ml$" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\(on\\)?$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.xml$" . nxml-mode))
 (add-to-list 'auto-mode-alist '("\\.html$" . html-mode))
+(add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-customization-version-id 190 t)
+ '(default-frame-alist (quote ((tool-bar-lines . 0) (menu-bar-lines . 1) (cursor-type . box) (vertical-scroll-bars . right) (internal-border-width . 0) (left-fringe . 1) (right-fringe) (fringe) (background-color . "black") (background-mode . dark) (border-color . "black") (cursor-color . "#FF9900") (foreground-color . "#CCCCCC") (mouse-color . "sienna1"))))
+ '(global-hl-line-mode t)
+ '(transient-mark-mode t))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
