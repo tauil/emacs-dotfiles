ccdacf84f5065e70814d4f6d659521818d755eea
Rafael B. Tauil <rafael@tauil.com.br>
2011-02-15 13:16:10 -0200

    Added rinari, bookmarks, autopair and some other minor improvements

--- custom.el
+++ custom.el
@@ -26,23 +26,34 @@
 (recentf-mode 1) ;; Save a list of recent files visited.
 (setq inhibit-splash-screen t) ;; Do not show splash screen
 (setq display-time-day-and-date t) (display-time) ;; Make the display of date and time persistent.
-(require 'linum) ;; Show line numbers
-(global-linum-mode 1)
-
-(defun fullscreen ()
-  (interactive)
-  (ns-toggle-fullscreen))
-(global-set-key "\M-m" 'fullscreen)
+(scroll-bar-mode nil)
 
 ;; Add libs to path -----------------------------------------------------------
 (add-to-list 'load-path "~/Projetos/emacs-designer-kit/lib")
+
+(require 'bm) 
+(require 'bookmark-add) 
+
+(require 'linum) ;; Show line numbers
+(global-linum-mode 1)
+
+;; Automaticaly pair braces
+(add-to-list 'load-path "/path/to/autopair") ;; comment if autopair.el is in standard load path 
+(require 'autopair)
+(autopair-global-mode) ;; enable autopair in all buffers 
+
+;;(require 'ack-emacs)
+
+;; Adding Rinari for Rails development
+(add-to-list 'load-path "~/Projetos/emacs-designer-kit/lib/rinari")
+(require 'rinari)
 
 ;; Add haml-mode
 (require 'haml-mode)
 
 ;; Add rvm  -------------------------------------------------------------------
-(require 'rvm)
-(rvm-use-default) ;; use rvm’s default ruby for the current Emacs session
+;;(require 'rvm)
+;;(rvm-use-default) ;; use rvm’s default ruby for the current Emacs session
 
 ;; Textmate mode --------------------------------------------------------------
 ;;(require 'textmate)
@@ -77,10 +88,45 @@
 
 ;;  Misc ----------------------------------------------------------------------
 (when window-system
+  ;; (setq frame-title-forma0t '(buffer-file-name "%f" ("%b")))
   (setq frame-title-format '(buffer-file-name "%f" ("%b")))
   (tooltip-mode -1)
   (tool-bar-mode -1)
   (blink-cursor-mode -1))
+
+;; Other keybinds
+(global-set-key [(control shift c)] 'comment-or-uncomment-region)
+(global-set-key "\M-g" 'goto-line)
+(defun fullscreen ()
+  (interactive)
+  (ns-toggle-fullscreen))
+(global-set-key "\M-m" 'fullscreen)
+
+;; Bookmark lines
+(global-set-key (kbd "<f2>") 'bm-toggle)
+(global-set-key (kbd "<C-f2>") 'bm-next)
+(global-set-key (kbd "<M-f2>") 'bm-previous)
+
+
+;; Rinari, Sane
+(define-prefix-command 'rinari-sane-map)
+(global-set-key (kbd "\C-c") 'rinari-sane-map)
+
+(define-key rinari-sane-map (kbd "f c") 'rinari-find-controller)
+(define-key rinari-sane-map (kbd "f f") 'rinari-find-file-in-project)
+(define-key rinari-sane-map (kbd "f h") 'rinari-find-helper)
+(define-key rinari-sane-map (kbd "f i") 'rinari-find-migration)
+(define-key rinari-sane-map (kbd "f j") 'rinari-find-javascript)
+(define-key rinari-sane-map (kbd "f l") 'rinari-find-lib)
+(define-key rinari-sane-map (kbd "f m") 'rinari-find-model)
+(define-key rinari-sane-map (kbd "f p") 'rinari-find-public)
+(define-key rinari-sane-map (kbd "f s") 'rinari-find-rspec)
+(define-key rinari-sane-map (kbd "f t") 'rinari-find-test)
+(define-key rinari-sane-map (kbd "f v") 'rinari-find-view)
+(define-key rinari-sane-map (kbd "f xt") 'rinari-find-by-context)
+(define-key rinari-sane-map (kbd "c") 'rinari-console)
+(define-key rinari-sane-map (kbd "g") 'rinari-rgrep)
+(define-key rinari-sane-map (kbd "t") 'rinari-test)
 
 ;; Associate modes with file extensions
 (add-to-list 'auto-mode-alist '("COMMIT_EDITMSG$" . diff-mode))
