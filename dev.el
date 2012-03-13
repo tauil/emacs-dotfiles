d4f761f730140959610364bdf3beb546bf09ec47
Rafael B. Tauil <rafael@tauil.com.br>
2012-03-13 11:17:04 -0300

    Adds custom snipptes to yasnippets

--- dev.el
+++ dev.el
@@ -1,6 +1,8 @@
 (add-to-list 'load-path "~/Projetos/yasnippet")
 (require 'yasnippet)
 (yas/global-mode 1)
+(setq yas/snippet-dirs "~/Projetos/emacs-dotfiles/snippets")
+(yas/load-directory yas/snippet-dirs)
 
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/Projetos/emacs-dotfiles/ac-dict")
