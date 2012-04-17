0f4b94a01bd30ec07fe10f9f8b04d6406cae3526
Rafael B. Tauil <rafael@tauil.com.br>
2012-04-17 15:46:11 -0300

    Improves emacs eshell and adds some aliases

--- eshell-setup.el
+++ eshell-setup.el
@@ -1,6 +1,16 @@
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+
 (if (eq system-type 'darwin)
     (setenv "PATH" "/usr/local/bin:/opt/local/bin:/opt/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin"))
+
+(setq eshell-aliases-file "~/Projetos/emacs-dotfiles/aliases")
+
 (defalias 'ff 'find-file)
 (defalias 'fo 'find-file-other-window)
-(defalias 'pj "cd ~/Projetos")
+
+(require 'ansi-color)
+    (require 'eshell)
+    (defun eshell-handle-ansi-color ()
+      (ansi-color-apply-on-region eshell-last-output-start
+                                  eshell-last-output-end))
+    (add-to-list 'eshell-output-filter-functions 'eshell-handle-ansi-color)
