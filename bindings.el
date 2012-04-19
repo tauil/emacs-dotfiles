0df0188262df3f231c363526f98ba6f02d7cd9fb
Rafael B. Tauil <rafael@tauil.com.br>
2012-04-19 10:39:18 -0300

    Changes file associations location file and adds Gemfile to ruby-mode

--- bindings.el
+++ bindings.el
@@ -34,17 +34,3 @@
 (global-set-key [(meta down)] 'textmate-column-down)
 (global-set-key [(meta shift up)] 'textmate-column-up-with-select)
 (global-set-key [(meta shift down)] 'textmate-column-down-with-select)
-
-;; Associate modes with file extensions
-(add-to-list 'auto-mode-alist '("COMMIT_EDITMSG$" . diff-mode))
-(add-to-list 'auto-mode-alist '("\\.css$" . css-mode))
-(add-to-list 'auto-mode-alist '("\\.ya?ml$" . yaml-mode))
-(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.yml$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.js\\(on\\)?$" . javascript-mode))
-(add-to-list 'auto-mode-alist '("\\.xml$" . nxml-mode))
-(add-to-list 'auto-mode-alist '("\\.html$" . html-mode))
-(add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
-(add-to-list 'auto-mode-alist '("profile" . shell-script-mode))
-(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
--- dev.el
+++ dev.el
@@ -15,3 +15,18 @@
 (ac-config-default)
 
 (require 'rainbow-mode) ;; For coloring hex codes
+
+;; Associate modes with file extensions
+(add-to-list 'auto-mode-alist '("COMMIT_EDITMSG$" . diff-mode))
+(add-to-list 'auto-mode-alist '("\\.css$" . css-mode))
+(add-to-list 'auto-mode-alist '("\\.ya?ml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.yml$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\(on\\)?$" . javascript-mode))
+(add-to-list 'auto-mode-alist '("\\.xml$" . nxml-mode))
+(add-to-list 'auto-mode-alist '("\\.html$" . html-mode))
+(add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
+(add-to-list 'auto-mode-alist '("profile" . shell-script-mode))
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
