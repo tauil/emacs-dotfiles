77e82af4a9a289a999869d9e93eaf81827891c64
Rafael B. Tauil <rafael@tauil.com.br>
2014-03-07 10:40:57 -0300

    Adds ruby-mode to puppet files

--- dev.el
+++ dev.el
@@ -34,6 +34,7 @@
 (add-to-list 'auto-mode-alist '("\\.yml$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.pp$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\(on\\)?$" . javascript-mode))
 (add-to-list 'auto-mode-alist '("\\.xml$" . nxml-mode))
 (add-to-list 'auto-mode-alist '("\\.html$" . html-mode))
