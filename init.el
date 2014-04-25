6cd4b495b3a2b32ff4ad5093519d17c98791d679
Rafael B. Tauil <rafael@tauil.com.br>
2014-04-25 10:45:44 -0300

    Adds more packages repo

--- init.el
+++ init.el
@@ -9,9 +9,9 @@
                      (convert-standard-filename "lib/")))
 
 (require 'package)
-(add-to-list 'package-archives
-    '("marmalade" .
-      "http://marmalade-repo.org/packages/"))
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
 (package-initialize)
 
 ;; Thanks to Jim Weirich
